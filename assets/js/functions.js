1800caaf06b4e4c33d565f0e72e0e26c086ae41f
unknown <hraj1@BLRHRAJ143349.sapient.com>
2015-07-20 20:25:46 +0530

    ajax content loaded

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,22 @@
 $(function(){
   smoothScroll(500);
   workBelt();
+  workLoad();
 });
+
+function workLoad(){
+    //Instructing the broser for cache settings and cache it
+    $.ajaxSetup({cache: true});
+    $('.thumb-unit').on('click',function(){
+      var $this = $(this),
+          newTitle = $this.find('strong').text(),
+          newContent = $this.data('folder'),
+          spinner = '<div class="loader">Loading...</div>',
+          newHTML = '/work/'+newContent+'.html';
+      $('.project-load').html(spinner).load(newHTML);
+      $('.project-title').text(newTitle);
+    });
+}
 
 function workBelt(){
     $('.thumb-unit').on('click',function(){
